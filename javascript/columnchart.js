2b43c1c503bfb28985efd95f75708e17d5fba5e5
calvinschmidt4 <calvinschmidt4@users.noreply.github.com>
2019-04-01 10:27:56 +0200

    Update columnchart.js

--- javascript/columnchart.js
+++ javascript/columnchart.js
@@ -1,15 +1,17 @@
-
-
+//ophalen van chart
 google.charts.load('current', {packages: ['corechart']})
 google.charts.setOnLoadCallback(drawChartColumn);
 
+// chart voor g-kracht
 function drawChartColumn() {
-     
+  
+  // opmaak en info voor de data in de chart
   var data = google.visualization.arrayToDataTable([
     ['Tijd', 'G-kracht', { role: 'style' }, {role: 'annotation'}, 'Over', { role: 'style' }],
     ['Huidig', 5, 'fill-color: #005288', '5G', 15, 'fill-color: #fff']
   ]);
 
+  // opmaak voor de chart
   var options = {  
     tooltip: {trigger: 'none'},            
     legend: 'none',
@@ -29,9 +31,12 @@ function drawChartColumn() {
     hAxis: {textPosition: 'none', minValue: 0},
     vAxis: {textPosition: 'none', minValue: 0, gridlines: {color: 'transparent'}, baselineColor: 'transparent'}
   };
+
+  // plaatsen van de chart met data en options op de pagina
   var chart = new google.visualization.ColumnChart(document.getElementById('columnchart'));
   chart.draw(data, options);      
 }
 
+// herschalen van de chart bij een resize van de browser
 $(window).resize(function(){drawChartColumn();});
 
